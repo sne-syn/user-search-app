d173e05e28bd65cda23174d154988a07d449cda1
sne_syn <sss@anadeainc.com>
2022-07-22 11:56:02 +0200

    Setup packeges' build

--- packages/react/src/atoms/Button/Button.tsx
+++ packages/react/src/atoms/Button/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
   label: string;
 }
 const Button: FC<ButtonProps> = ({ label }) => {
-  return <button>{label}</button>;
+  return <button className="dse-button__container">{label}</button>;
 };
 
 export default Button;
