04b84ee7a46966f0f80277329d6458eedc47ee36
sne_syn <sss@anadeainc.com>
2022-07-22 12:48:10 +0200

    Structure updates

--- playgrounds/react/dist/src.f69400ca.js
+++ playgrounds/react/dist/src.f69400ca.js
@@ -33263,7 +33263,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63807" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59681" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
